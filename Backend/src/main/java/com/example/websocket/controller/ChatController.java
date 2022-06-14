e1bfa1191cbda7c7ef321981308591bfdaf28882
Ducilio <62483330+DucilioManjate@users.noreply.github.com>
2022-06-14 12:51:27 -0300

    finish

--- Backend/src/main/java/com/example/websocket/controller/ChatController.java
+++ Backend/src/main/java/com/example/websocket/controller/ChatController.java
@@ -3,6 +3,8 @@ package com.example.websocket.controller;
 
 
 import com.example.websocket.model.Message;
+import com.example.websocket.repositories.MessageRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,20 +12,27 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+@Slf4j
 @Controller
 public class ChatController {
+
+    @Autowired
+    private MessageRepository messageRepository;
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
     @MessageMapping("/message")
     @SendTo("/chatroom/public")
     public Message receiveMessage(@Payload Message message){
+        log.info("uma mensagem publica recebida: {}", message);
+        messageRepository.save(message);
         return message;
     }
     @MessageMapping("/private-message")
     public Message recMessage(@Payload Message message){
-        simpMessagingTemplate.convertAndSendToUser(message.getMessage(),"/private",message);
-        System.out.println(message.toString());
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(),"/private",message);
+        log.info("uma mensagem privada para {}, payload: {}", message.getReceiverName(),message.getMessage());
+        messageRepository.save(message);
         return message;
     }
 
--- Backend/src/main/java/com/example/websocket/model/Message.java
+++ Backend/src/main/java/com/example/websocket/model/Message.java
@@ -1,17 +1,22 @@
 package com.example.websocket.model;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
+@Document(collation = "messages")
 public class Message {
-    private String name;
-    private String Cellphone;
-    private String email;
+    @Id
+    private String id;
+    private String senderName;
+    private String receiverName;
     private String message;
+    private String date;
     private Status status;
 
 }
