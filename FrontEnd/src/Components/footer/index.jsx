46d47e5b15035fcfe76bce4109e0011af3556ada
Ducilio <62483330+DucilioManjate@users.noreply.github.com>
2022-06-13 22:29:42 -0300

    my 2 commit

---
+++ FrontEnd/src/Components/footer/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './index.css'
+
+const index = () => (
+  <div className="footer">
+    <p className="footer-copyright">
+      &copy; 
+        Entrega IV Engenharia de software| UniAmerica +Descomplica. {new Date().getFullYear()}
+    </p>
+  </div>
+)
+
+export default index;
